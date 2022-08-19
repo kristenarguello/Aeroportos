e80287f94d577f6c33ccf39a5b5cac5a108b7fab
Kristen <kristenarguello20@gmail.com>
2022-08-19 18:35:25 -0300

    testePull

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -8,4 +8,16 @@ public class GerenciadorCias {
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void adicionar(CiaAerea cia) {
+		empresas.add(cia);
+	}
+	public CiaAerea buscaPorCodigo (String cod) {
+		for (int i=0;i<empresas.size();i++) {
+			CiaAerea a = empresas.get(i);
+			if (a.getCodigo().equals(cod))
+				return a;
+		}
+		return null;
+	}
 }
