176cfcc99aea7033c47b73466ab4d2c77091d3aa
Kristen <kristenarguello20@gmail.com>
2022-09-01 18:18:06 -0300

    Ex2

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -9,17 +9,18 @@ public class App {
 		System.out.println("\nMyFlight project...");
 		
 		//teste - ex1
-		Geo locala = new Geo(-29.9942,-51.1714);
-		Geo localb = new Geo(-23.4322,-46.4692);
+		Geo local_poa = new Geo(-29.9942,-51.1714);
+		Geo local_sp = new Geo(-23.4322,-46.4692);
 		CiaAerea cia1 = new CiaAerea("123","Gol");
-		Aeroporto aeroporto_ori1 = new Aeroporto("456", "Salgado Filho", locala);
-		Aeroporto aeroporto_che1 = new Aeroporto("789","Guarulhos",localb);
+		Aeroporto aeroporto_ori1 = new Aeroporto("456", "Salgado Filho", local_poa);
+		Aeroporto aeroporto_che1 = new Aeroporto("789","Guarulhos",local_sp);
 		Aeronave aeronave1 = new Aeronave("7979", "internacional");
 		Rota rota1 = new Rota(cia1, aeroporto_ori1, aeroporto_che1, aeronave1);
 		
 		Voo voo_teste1 = new Voo(rota1, Duration.ofHours(4));
 		Voo voo_teste2 = new Voo(rota1, LocalDateTime.of(2018, Month.SEPTEMBER, 01, 12,00), Duration.ofHours(4));
 		
-
+		//teste - ex2
+		System.out.println("");
 	}
 }
--- src/pucrs/myflight/modelo/Geo.java
+++ src/pucrs/myflight/modelo/Geo.java
@@ -16,4 +16,30 @@ public class Geo {
 	public double getLongitude() {
 		return longitude;
 	}
+
+	public static double distancia(Geo um, Geo dois) {
+		double e = (Math.toRadians(um.getLatitude())-Math.toRadians(dois.getLatitude()))/2;
+		double c = Math.pow(Math.sin(e),2);
+
+		double d = (Math.toRadians(um.getLongitude()) - Math.toRadians(dois.getLongitude()))/2;
+		double f = Math.pow(Math.sin(d),2) * Math.cos(Math.toRadians(um.getLatitude())) * Math.cos(Math.toRadians(dois.getLatitude()));
+		
+		double b = Math.sqrt(c+f);
+		double a = 2*(6371)*Math.asin(b);
+		return a;
+	}
+
+	public double distancia(Geo geo) {
+		double e = (Math.toRadians(geo.getLatitude())-Math.toRadians(latitude))/2;
+		double c = Math.pow(Math.sin(e),2);
+
+		double d = (Math.toRadians(geo.getLongitude()) - Math.toRadians(longitude))/2;
+		double f = Math.pow(Math.sin(d),2) * Math.cos(Math.toRadians(geo.getLatitude())) * Math.cos(Math.toRadians(latitude));
+		
+		double b = Math.sqrt(c+f);
+		double a = 2*(6371)*Math.asin(b);
+		return a;
+	}
+
+	
 }
