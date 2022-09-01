9969286331a64492b4f49450215d11dd070c7bd9
Kristen <kristenarguello20@gmail.com>
2022-09-01 19:28:07 -0300

    Commit-todo-mundo-junto

---
+++ src/pucrs/myflight/modelo/VooEscalas.java
@@ -0,0 +1,32 @@
+package pucrs.myflight.modelo;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class VooEscalas extends Voo {
+    private Rota rotaFinal;
+
+    public VooEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao) {
+        super(rota, datahora, duracao); //chama o contrutor do Voo
+        this.rotaFinal = rotaFinal;
+    }
+
+    public Rota getRotaFinal() {
+        return rotaFinal;
+    }
+
+/*
+    @Override
+    public String toString() {
+        //return status + " " + datahora + "("+duracao+"): "+rota+ "-->" + rotaFinal;
+        return getStatus() + " " + getDataHora() + "("+getDuracao()+"): " + getRota() + " -> " +rotaFinal;
+    }
+ */ 
+
+    @Override
+    public String toString() {
+        return super.toString() + " -> " + rotaFinal;
+    }
+
+
+}
---
+++ src/pucrs/myflight/modelo/VooVariasEscalas.java
@@ -0,0 +1,13 @@
+package pucrs.myflight.modelo;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class VooVariasEscalas extends VooEscalas{
+
+    public VooVariasEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao) {
+        super(rota, rotaFinal, datahora, duracao);
+    
+    }
+    
+}
