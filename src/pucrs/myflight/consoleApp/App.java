d18ec7c9d991fd1dde9e9d856e941df5c5b37866
Kristen <kristenarguello20@gmail.com>
2022-09-01 17:49:18 -0300

    Teste_ex1

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -1,9 +1,25 @@
 package pucrs.myflight.consoleApp;
+import java.time.*;
+
+import pucrs.myflight.modelo.*;
 
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("\nMyFlight project...");
-		//tem que testar os construtores do voo
+		
+		//teste - ex1
+		Geo locala = new Geo(-29.9942,-51.1714);
+		Geo localb = new Geo(-23.4322,-46.4692);
+		CiaAerea cia1 = new CiaAerea("123","Gol");
+		Aeroporto aeroporto_ori1 = new Aeroporto("456", "Salgado Filho", locala);
+		Aeroporto aeroporto_che1 = new Aeroporto("789","Guarulhos",localb);
+		Aeronave aeronave1 = new Aeronave("7979", "internacional");
+		Rota rota1 = new Rota(cia1, aeroporto_ori1, aeroporto_che1, aeronave1);
+		
+		Voo voo_teste1 = new Voo(rota1, Duration.ofHours(4));
+		Voo voo_teste2 = new Voo(rota1, LocalDateTime.of(2018, Month.SEPTEMBER, 01, 12,00), Duration.ofHours(4));
+
+		
 	}
 }
