32515cb938591a3229965ab5531e13f199238ac2
Carolina Shypelenko Kowaluk <110862002+carolina-kowaluk@users.noreply.github.com>
2022-09-13 21:32:41 -0300

    Update GerenciadorRotas.java

--- src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -5,6 +5,30 @@ import java.util.Collections;
 public class GerenciadorRotas {
  private ArrayList<Rota> rotas;
 
+public void adicionar(Rota rota) {
+        rotas.add(rota);
+}
+
+public void listarTodas() {
+   System.out.println("Rotas:");
+      for (Rota r : rotas) {
+          System.out.println(r.getCia()+" - ");
+          System.out.print(r.getOrigem()+" - ");
+          System.out.print(r.getDestino()+" - ");
+          System.out.println(r.getAeronave());
+      }     
+}
+
+public Rota buscarPorOrigem(Aeroporto origem) {
+    return null;
+
+}
+
+ public Rota buscarPorDestino(Aeroporto destino) {
+    return null;
+
+}
+ 
  public void ordenaRotas() {
     Collections.sort(rotas);
  }
