f2316a56ad5f347bf84a88233771b02bf306c226
Carolina Shypelenko Kowaluk <110862002+carolina-kowaluk@users.noreply.github.com>
2022-09-27 15:52:11 -0300

    Update Voo.java

--- src/pucrs/myflight/modelo/Voo.java
+++ src/pucrs/myflight/modelo/Voo.java
@@ -1,18 +1,23 @@
 package pucrs.myflight.modelo;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
+
 public abstract class Voo {
     public enum Status { CONFIRMADO, ATRASADO, CANCELADO };
 
     private LocalDateTime datahora;
+    private Status status;
 
     public Voo(LocalDateTime datahora) {
-        //??
-    }
+        this.datahora = datahora;
+        this.status = Status.CONFIRMADO;
+    } 
     public LocalDateTime getDataHora() {return datahora;}
     public abstract Rota getRota();
     public abstract Duration getDuracao();
 
+    public String toString () {
+        return status + " " + datahora; //duracao: rota
+    }
     
 }
