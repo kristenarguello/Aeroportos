5fd9206382c0ce3a5eb11df99f9f6f9426b0f903
Carolina Shypelenko Kowaluk <110862002+carolina-kowaluk@users.noreply.github.com>
2022-09-27 15:52:49 -0300

    Update VooDireto.java

--- src/pucrs/myflight/modelo/VooDireto.java
+++ src/pucrs/myflight/modelo/VooDireto.java
@@ -2,52 +2,48 @@ package pucrs.myflight.modelo;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.Month;
 
-public class VooDireto {
+
+public class VooDireto extends Voo{
 	
 	public enum Status { CONFIRMADO, ATRASADO, CANCELADO };
 	
-	private LocalDateTime datahora;
+	//private LocalDateTime datahora;
 	private Duration duracao;
 	private Rota rota;
-	private Status status;
+	//private Status status;
 	
-	public VooDireto(Rota rota, LocalDateTime datahora, Duration duracao) {
+	public VooDireto(Rota rota, LocalDateTime datahora) {
+		super(datahora);
 		this.rota = rota;
-		this.datahora = datahora;
-		this.duracao = duracao;
-		this.status = Status.CONFIRMADO; // default é confirmado
+		
+		//this.datahora = datahora;
+		//this.status = Status.CONFIRMADO; // default é confirmado
 	}
-	public VooDireto(Rota rota,Duration duracao) {
+	/* SEGUNDO CONSTRUTOR - PRIMEIRO EXERCICIO
+	public VooDireto(Rota rota) {
+
 		this.rota = rota;
-		this.duracao = duracao;
 		this.datahora = LocalDateTime.of(2016, Month.AUGUST, 12, 12,00); //default
-		this.status = Status.CONFIRMADO; //default
+		//this.status = Status.CONFIRMADO; //default
 	}
-	
+	*/
+	@Override
 	public Rota getRota() {
 		return rota;
 	}
 	
-	public LocalDateTime getDatahora() {
-		return datahora;
-	}
-	
+	@Override
 	public Duration getDuracao() {
-		return duracao;
+		//t = v/d
+		double min =(805/Geo.distancia(rota.getDestino().getLocal(), rota.getOrigem().getLocal()))*60 + 30;
+		return Duration.ofMinutes((long)min);
 	}
 	
-	public Status getStatus() {
-		return status;
-	}
 	
-	public void setStatus(Status novo) {
-		this.status = novo;
-	}
 
 	public String toString() {
-		return status + " " + datahora + "("+duracao+"): " + rota;
+		return super.toString() + "("+duracao+"): " + rota;
 	}
 
 	
